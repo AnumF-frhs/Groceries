38eafab6be7ca3b2dca0b4b24a3e33991a8f6a52
Anum <127afarghani@frhsd.com>
2026-02-04 13:36:48 -0500

    3

--- js/grocery.js
+++ js/grocery.js
@@ -9,12 +9,15 @@ var milk = 3.49;
 var eggs = 4.25;
 
 
-var cartTotal = 0 // didn't put anything in cart
+var cartTotal = 0; // didn't put anything in cart
 
 
-function addToCart (item1) {
-
+function addToCart (item) {
+    //no return function
+    cartTotal = item  + cartTotal; //doesn't matter what item b/c we have values
+    //console.log(addToCart(banana)), leads to undefined because there's no return keyword. log cartTotal..
 };
 
-addToCart();
-console.log(addToCart());
+addToCart(banana + apple + milk *3);
+console.log(cartTotal);
+// it logged
