3e91316c4d669c5579458198077401c5c94965d9
Anum <127afarghani@frhsd.com>
2026-02-04 13:27:48 -0500

    jsb2

--- js/grocery.js
+++ js/grocery.js
@@ -1,7 +1,20 @@
 alert('meow');
 
-let banana = 0.79;
-let apple = 1.25;
-let bread = 2.49;
-let milk = 3.49;
-let eggs = 4.25;
+//stuffzies
+
+var banana = 0.79;
+var apple = 1.25;
+var bread = 2.49;
+var milk = 3.49;
+var eggs = 4.25;
+
+
+var cartTotal = 0 // didn't put anything in cart
+
+
+function addToCart (item1) {
+
+};
+
+addToCart();
+console.log(addToCart());
